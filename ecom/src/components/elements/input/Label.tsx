5e884ca0a891e8e85ddde6a460133206d9eae807
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-19 17:06:58 +0700

    Merubah properti htmlFor jadi optional dan menambah properti key dan className

--- ecom/src/components/elements/input/Label.tsx
+++ ecom/src/components/elements/input/Label.tsx
@@ -3,17 +3,21 @@ import React from 'react'
 // definisikan tipe untuk properti props
 type LabelProps = {
     children: React.ReactNode;
-    htmlFor: string;
+    htmlFor?: string;
+    key?: React.Key; // Tambahkan tipe React.Key
+    className?: string; 
 }
 
 const Label: React.FC<LabelProps> = ({
     children,
-    htmlFor
+    htmlFor,
+    className
+    // key, // Tidak perlu diambil karena 'key' tidak digunakan dalam JSX
 }) => {
   return (
     <label
         htmlFor={htmlFor}
-        className="block text-slate-700 text-sm font-bold mb-2"
+        className={`block text-slate-700 text-sm font-bold mb-2 ${className}`}
     >
         {children}
     </label>
