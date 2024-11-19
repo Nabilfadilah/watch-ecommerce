176b24d8de07b3793b9280bf1648ce28b66f74dc
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-19 14:27:30 +0700

    Menambahkan element input search

---
+++ ecom/src/components/elements/input/InputSearch.tsx
@@ -0,0 +1,36 @@
+import React, { forwardRef, ForwardedRef } from "react";
+import { AiOutlineSearch } from "react-icons/ai"; // Import ikon dari React Icons
+
+// Menentukan tipe data untuk properti
+type InputSearchProps = {
+  placeholder?: string;
+  name: string;
+  className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // memastikan fungsi handler menerima event yang sesuai
+};
+
+// digunakan untuk mengarahkan ref ke elemen input HTML.
+const InputSearch = forwardRef<HTMLInputElement, InputSearchProps>(
+  // placeholder properti default Cari..
+  ({ placeholder = "Cari...", name, className, onChange }, ref: ForwardedRef<HTMLInputElement>) => {
+    return (
+      <div className="relative w-80">
+        {/* Ikon Search */}
+        <AiOutlineSearch className="absolute left-3 top-2.5 text-gray-400" />
+
+        {/* Input Field */}
+        <input
+          type="search"
+          className={`text-sm border border-gray-500 rounded w-full py-2 px-10 text-slate-700 placeholder:opacity-90 ${className}`}
+          placeholder={placeholder}
+          name={name}
+          id={name}
+          ref={ref}
+          onChange={onChange}
+        />
+      </div>
+    );
+  }
+);
+
+export default InputSearch;
