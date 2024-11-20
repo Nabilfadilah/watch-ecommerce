4532c292c807fcaa49d356327c043de14b89a628
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-20 10:33:32 +0700

    Menambah route product page

--- ecom/src/App.tsx
+++ ecom/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Sidebar from "./components/layouts/Sidebar"
 import MainContent from "./components/layouts/MainContent"
+import ProductPage from "./components/layouts/ProductPage"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <div className="rounded w-full flex justify-between flex-wrap">
           <Routes>
             <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
         </div>
       </div>
