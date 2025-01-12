215b8aa7dd0aec7ddf6681154446397a8e26d9ad
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-12 21:18:22 +0700

    Memberikan hover, warna border, dan dollar untuk mapping price

--- ecom/src/components/fragments/BookCard.tsx
+++ ecom/src/components/fragments/BookCard.tsx
@@ -1,24 +1,28 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom";
 import Typography from "../elements/text/Typography";
 
 interface BookCardProps {
-    id: string;
-    title: string;
-    image: string;
-    price: number;
+  id: string;
+  title: string;
+  image: string;
+  price: number;
 }
 
-const BookCard: React.FC<BookCardProps> = ({ id, title, image, price }) => {
-    return (
-        <div className="border p-4 rounded">
-            <Link to={`/product/${id}`}>
-                <img src={image} alt={title} className="w-full h-32 object-cover mb-2" />
+const BookCard: React.FC<BookCardProps> = ({id, title, image, price}) => {
+  return (
+    <div className="border border-gray-500 p-4 rounded hover:bg-gray-100">
+      <Link to={`/product/${id}`}>
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-32 object-cover mb-2"
+        />
 
-                <Typography className="font-bold">{title}</Typography>
-                <Typography>{price}</Typography>
-            </Link>
-        </div>
-    )
-}
+        <Typography className="font-bold">{title}</Typography>
+        <Typography>$ {price}</Typography>
+      </Link>
+    </div>
+  );
+};
 
-export default BookCard
+export default BookCard;
